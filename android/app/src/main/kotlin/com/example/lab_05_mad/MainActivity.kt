efe9baad79d53e9dc13b4890254f44ba2cfbd7e9
zionmezba <mezbazion@gmail.com>
2025-02-16 09:22:48 +0600

    initiate

---
+++ android/app/src/main/kotlin/com/example/lab_05_mad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab_05_mad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppHome());
+}
+
+class AppHome extends StatelessWidget {
+  const AppHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lab 05 - Flutter"),
+        ),
+        body: Text("This is from body"),
+      ),
+    );
+  }
+}
