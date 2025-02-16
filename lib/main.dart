00d17ec91610715dafed12d62fea4fb57cac48dd
zionmezba <mezbazion@gmail.com>
2025-02-16 12:57:26 +0600

    tgh

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,95 @@ class AppHome extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Lab 05 - Flutter"),
+          elevation: 10,
+          shadowColor: Colors.grey[200],
+          backgroundColor: const Color.fromARGB(255, 255, 160, 7),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-              size: 40,
-              Iconsax.activity4,
-              color: Colors.blue,
-            ),
-            Icon(
-              size: 40,
-              Iconsax.activity4,
-              color: Colors.blue,
-            ),
-            Text(
-              "This is a ",
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
+        body: Container(
+          height: 480,
+          margin: EdgeInsets.all(30),
+          padding: EdgeInsets.all(15),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 30,
+                color: Colors.grey,
+              )
+            ],
+            color: Color.fromARGB(255, 240, 240, 240),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(5, 7, 5, 7),
+                    margin: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                    ),
+                    child: Text(
+                      "50% OFF",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Iconsax.heart_circle4,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                ],
               ),
-            ),
-            Icon(
-              size: 30,
-              Iconsax.add_circle,
-              color: Colors.blue,
-            ),
-          ],
+              Image(image: AssetImage('assets/images/image01.png')),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Nike Air Shoes",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "\$450",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        "\$500",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          decorationColor: Colors.grey,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
